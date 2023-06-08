fc3ced012292680044179e2f140e85ed5401ce79
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-08 17:49:27 +0530

    routes file for expense app

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express');
+
+const router = express.Router();
+
+const expenseController = require('../controllers/expense');
+
+
+router.post('/add-expense',expenseController.postExpense);
+
+router.get('/get-expense',expenseController.getExpense);
+
+router.delete('/delete-expense/:id',expenseController.deleteExpense);
+
+router.put('/edit-expense/:id',expenseController.editExpense);
+
+module.exports = router;
